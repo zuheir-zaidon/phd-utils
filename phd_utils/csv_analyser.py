1b3e2f6dd591bfce505df3a18d436be479ffd6b9
Aatif Syed <aatifsyedyp@gmail.com>
2021-05-25 23:32:28 +0100

    cli for csv-analyser

--- phd_utils/csv_analyser.py
+++ phd_utils/csv_analyser.py
@@ -10,9 +10,10 @@ logger = logging.getLogger(__name__)
 
 def read_displacement_csv(path: Path):
     """Reads in a CSV, returning a dataframe with:
-    - Frame (index)
-    - X_Displacement
-    - Y_Displacement
+    - (Index)
+    - Frame
+    - X_Position
+    - Y_Position
 
     Args:
         path (Path): Where the file is
@@ -40,13 +41,13 @@ def read_displacement_csv(path: Path):
     df.rename(
         columns={
             "H": "Frame",
-            "E": "X_Displacement",
-            "F": "Y_Displacement",
+            "E": "X_Position",
+            "F": "Y_Position",
         },
         inplace=True,
     )
 
-    # Pandas interpreted this as a float. Fix that now, and set it as the index
+    # Pandas interpreted this as a float. Fix that now
     df["Frame"] = df["Frame"].astype(int)
 
     df.drop(
@@ -56,12 +57,14 @@ def read_displacement_csv(path: Path):
         inplace=True,
     )
 
+    logger.info(f"Loaded csv from {path.as_posix()} ({len(df)} rows)")
+
     return df
 
 
 def merge_and_displace_frames(
-    reference: pd.DataFrame,
     substrate: pd.DataFrame,
+    reference: pd.DataFrame,
     pipette: pd.DataFrame,
     experiment_duration: pd.Timedelta,
     duration_of_resampled_row: pd.Timedelta,
@@ -96,12 +99,15 @@ def merge_and_displace_frames(
         columns=[col for col in combined.columns if col.endswith("Frame")],
         inplace=True,
     )
+    logger.info(
+        f"Resampled to buckets of {duration_of_resampled_row} ({len(combined)} rows)"
+    )
 
     combined["X_Delta"] = (
-        combined["Substrate_X_Displacement"] - combined["Reference_X_Displacement"]
+        combined["Substrate_X_Position"] - combined["Reference_X_Position"]
     )
     combined["Y_Delta"] = (
-        combined["Substrate_Y_Displacement"] - combined["Reference_Y_Displacement"]
+        combined["Substrate_Y_Position"] - combined["Reference_Y_Position"]
     )
 
     # Make our delta lines start at 0
@@ -125,7 +131,7 @@ def generate_normal_force_and_correct_for_load_positioning(
 ):
     displaced_x_delta = df["X_Delta"] + initial_x_displacement
     bead_to_tip_displacement = (
-        substrate_tip_position - df["Substrate_Y_Displacement"].iloc[0]
+        substrate_tip_position - df["Substrate_Y_Position"].iloc[0]
     )
     length_from_the_tip = length_of_substrate - bead_to_tip_displacement
     df["Corrected_Deflection"] = (
@@ -136,11 +142,15 @@ def generate_normal_force_and_correct_for_load_positioning(
 
     # TODO: statistically sound heuristic here!
     if pipette_position_at_rest is None:
-        pipette_position_at_rest = df["Pipette_Y_Displacement"][
+        pipette_position_at_rest = df["Pipette_Y_Position"][
             : pd.Timedelta(3, "seconds")
         ].mean()
 
-    df["Pipette_Deflection"] = df["Pipette_Y_Displacement"] - pipette_position_at_rest
+        logger.info(
+            f"Guessing pipette position at rest based on first 3 seconds of measurement (got {pipette_position_at_rest})"
+        )
+
+    df["Pipette_Deflection"] = df["Pipette_Y_Position"] - pipette_position_at_rest
 
     df["Friction_Force"] = df["Pipette_Deflection"] * stiffness_constant_of_pipette
 
@@ -153,12 +163,61 @@ def main():
     parser = argparse.ArgumentParser(
         description="""
     Given a list of directories this program will, for each directory:
-    - Read substrate*.csv and reference*.csv
-
+    - Read substrate*.csv, reference*.csv and pipette*.csv
+    - Convert them to timeseries, and concatenate
+    - Resample
+    - Do some basic analysis
     """,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument("folder", type=Path, nargs="+")
+    parser.add_argument(
+        "folder",
+        type=Path,
+        help="""The name of the folder containing the three csvs, with names containing either "reference", "substrate" or "pipette" (one of each)""",
+    )
+    parser.add_argument(
+        "-e",
+        "--experiment-duration",
+        type=float,
+        help="Duration of this experiment, in seconds",
+        required=True,
+    )
+    parser.add_argument(
+        "-r",
+        "--resample-to",
+        type=float,
+        help="How many seconds each row should last for after resampling",
+        required=True,
+    )
+
+    parser.add_argument("-x", "--initial-x-displacement", type=float, required=True)
+    parser.add_argument("-t", "--substrate-tip-position", type=float, required=True)
+    parser.add_argument("-L", "--substrate-length", type=float, required=True)
+    parser.add_argument("-s", "--substrate-stiffness", type=float, required=True)
+    parser.add_argument("-p", "--pipette-stiffness", type=float, required=True)
+    parser.add_argument("-R", "--pipette-position-at-rest", type=float, required=True)
+    parser.add_argument(
+        "-o",
+        "--output-file-name",
+        type=str,
+        default="processed.csv",
+        help="The name of the file to save the results to. Defaults to `processed.csv`",
+    )
+    parser.add_argument(
+        "-O",
+        "--overwrite",
+        default=False,
+        action="store_true",
+        help="If the output filename already exists, ovewrite it. Else, the program will raise an error",
+    )
+    parser.add_argument(
+        "-H",
+        "--hdf",
+        default=False,
+        action="store_true",
+        help="Also save as a `processed.hdf` file, for native pandas loading.",
+    )
+
     parser.add_argument(
         "-l",
         "--log-level",
@@ -171,3 +230,49 @@ def main():
     logging.basicConfig(level=args.log_level)
 
     logger.debug(f"Arguments: {args}")
+    folder: Path = args.folder
+
+    def glob_once(folder: Path, pattern: str):
+        candidates = list(folder.glob(pattern))
+        assert len(candidates) == 1, f"Found more than file for {pattern}: {candidates}"
+        destination_path = candidates.pop()
+        assert destination_path.is_file()
+        logging.info(f"Using {destination_path.as_posix()}")
+        return destination_path
+
+    substrate_path = glob_once(folder, "*[sS]ubstrate*.csv")
+    reference_path = glob_once(folder, "*[rR]eference*.csv")
+    pipette_path = glob_once(folder, "*[pP]ipette*.csv")
+
+    merged_and_displaced = merge_and_displace_frames(
+        substrate=read_displacement_csv(substrate_path),
+        reference=read_displacement_csv(reference_path),
+        pipette=read_displacement_csv(pipette_path),
+        experiment_duration=pd.Timedelta(
+            value=args.experiment_duration, unit="seconds"
+        ),
+        duration_of_resampled_row=pd.Timedelta(value=args.resample_to, unit="seconds"),
+    )
+
+    result = generate_normal_force_and_correct_for_load_positioning(
+        df=merged_and_displaced,
+        initial_x_displacement=args.initial_x_displacement,
+        substrate_tip_position=args.substrate_tip_position,
+        length_of_substrate=args.substrate_length,
+        stiffness_constant_of_substrate=args.substrate_stiffness,
+        stiffness_constant_of_pipette=args.pipette_stiffness,
+        pipette_position_at_rest=args.pipette_position_at_rest,
+    )
+
+    output_file = folder.joinpath(args.output_file_name)
+    if output_file.exists():
+        assert output_file.is_file()
+        assert (
+            args.overwrite
+        ), f"About to write over existing file {output_file}, but `--overwrite` not specified"
+        logger.warn(f"Overwriting file {output_file.as_posix()}")
+
+    result.to_csv(output_file)
+
+    if args.hdf:
+        result.to_hdf(folder.joinpath("processed.hdf"), key="processed")
